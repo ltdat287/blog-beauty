9fb1e4fac825b9a71f2d33ffae9e8624c495f2f8
ltdat287 <ltdat5789@gmail.com>
2015-07-29 00:17:03 +0700

    [finished] Add CRUD for TagController from admin page

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\TagCreateRequest;
+use App\Http\Requests\TagUpdateRequest;
 use App\Http\Controllers\Controller;
 use App\Tag;
 
@@ -17,7 +19,7 @@ class TagController extends Controller
             'meta_description' => '',
             'page_image' => '',
             'layout' => 'blog.layouts.index',
-            'reverse_directory' => 0,
+            'reverse_direction' => 0,
         ];
     /**
      * Display a listing of the resource.
@@ -54,9 +56,16 @@ class TagController extends Controller
      * @param  Request  $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store(TagCreateRequest $request)
     {
         //
+        $tag = new Tag();
+        foreach (array_keys($this->fields) as $field) {
+            $tag->$field = $request->get($field);
+        }
+        $tag->save();
+
+        return redirect('/admin/tag')->withSuccess("The tag '$tag->tag' was created.");
     }
 
     /**
@@ -79,6 +88,13 @@ class TagController extends Controller
     public function edit($id)
     {
         //
+        $tag = Tag::findOrFail($id);
+        $data = ['id' => $id];
+        foreach (array_keys($this->fields) as $field) {
+            $data[$field] = old($field, $tag->$field);
+        }
+
+        return view('admin.tag.edit', $data);
     }
 
     /**
@@ -88,9 +104,16 @@ class TagController extends Controller
      * @param  int  $id
      * @return Response
      */
-    public function update(Request $request, $id)
+    public function update(TagUpdateRequest $request, $id)
     {
         //
+        $tag = Tag::findOrFail($id);
+        foreach (array_keys(array_except($this->fields, ['tag'])) as $field) {
+            $tag->$field = $request->get($field);
+        }
+        $tag->save();
+
+        return redirect("/admin/tag/$id/edit")->withSuccess("Changes saved.");
     }
 
     /**
@@ -102,5 +125,9 @@ class TagController extends Controller
     public function destroy($id)
     {
         //
+        $tag = Tag::findOrFail($id);
+        $tag->delete();
+
+        return redirect('/admin/tag')->withSuccess("The '$tag->tag' tag has veen deleted.");
     }
 }
